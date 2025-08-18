068868a16214029d536be5f71e3dd0621dae80e0
maxy <seriousmaxsteel@gmail.com>
2025-08-18 21:25:41 +0700

    fix 3

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # wsgi.py (repo root)
 from .app import app  # adjust if your inner package is named differently
-q
+
 # optional: for local run `python wsgi.py`
 if __name__ == "__main__":
     app.run()
